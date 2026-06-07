3cb412cc02afd521120888d66274cbd8804d4914
coder sohil <jatinsangwan1582006@gmail.com>
2026-06-07 13:02:39 +0530

    background.js
    Set up a listener to analyze URLs when pages finish loading.

---
+++ background.js
@@ -0,0 +1,8 @@
+javascript
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Triggers when a page finishes loading
+  if (changeInfo.status === 'complete' && tab.url) {
+    console.log("PhishGuard background worker analyzing URL: " + tab.url);
+    // This is where we will eventually inject our ONNX ML model inference pipeline!
+  }
+});
