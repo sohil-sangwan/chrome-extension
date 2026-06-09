9988e197be2507acece4a814e140582adb5aeeb6
Anjali Sinha <anjalisinha0217@gmail.com>
2026-06-09 14:15:49 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-javascript
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // Triggers when a page finishes loading
-  if (changeInfo.status === 'complete' && tab.url) {
-    console.log("Byteshield background worker analyzing URL: " + tab.url);
-    // This is where we will eventually inject our ONNX ML model inference pipeline!
-  }
-});
-
